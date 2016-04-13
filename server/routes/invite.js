68a7939bfe922d836d8735db66b28e8b9d20198d
June Shaw <june.shaw@me.com>
2016-04-12 20:18:50 -0600

    rsvps

--- server/routes/invite.js
+++ server/routes/invite.js
@@ -64,7 +64,7 @@ router.post('/receive', function(req, res, next) {
   console.log('Received a message from Twilio: ', req.body.Body, req.body.From);
   console.log('not reaching it?');
   var phone = parseString(req.body.From);
-  db.memberByPhone(req.body.From).then(function(data) {
+  db.memberByPhone().then(function(data) {
     console.log('data: ', data);
     db.activityMemberByActMem(req.params.id, req.params.memberId, req.params.reply).then(function(data) {
       console.log('@@@@@@ rsvp data: ', data);
