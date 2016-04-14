234c9c2093f4643d32664c3ced7fd2b5e38c0da5
June Shaw <june.shaw@me.com>
2016-04-14 03:01:24 -0600

    fix

--- server/src/create.js
+++ server/src/create.js
@@ -44,8 +44,7 @@ module.exports = {
       image_url: activity.image_url,
       category_name: category_filter
     }).then(function(data) {
-      console.log('new activity data: ', data[0]);   // need the id back
-        // db.initializeActivityRsvp(group, activityId)
+        console.log('new activity data: ', data[0]);   // need the id back
         console.log('ACTIVITY: ', activity);
         console.log('GROUP: ', group);
         console.log('TIME: ', time.time);
@@ -61,64 +60,63 @@ module.exports = {
         })
       })
     }
-    }
+  };
 
 
-    var request_yelp = function(set_parameters, callback) {
+  var request_yelp = function(set_parameters, callback) {
 
       /* The type of request */
-      var httpMethod = 'GET';
+    var httpMethod = 'GET';
 
       /* The url we are using for the request */
-      var url = 'http://api.yelp.com/v2/search';
+    var url = 'http://api.yelp.com/v2/search';
 
       /* We can setup default parameters here */
-      var default_parameters = {
-        location: 'Denver+CO',
-        sort: '2'
-      };
+    var default_parameters = {
+      location: 'Denver+CO',
+      sort: '2'
+    };
 
-      /* We set the require parameters here */
-      var required_parameters = {
-        // oauth_consumer_key : process.env.oauth_consumer_key,
-        oauth_consumer_key : process.env.YELP_CONSUMER_KEY,
-        oauth_token : process.env.YELP_TOKEN,
-        oauth_nonce : n(),
-        oauth_timestamp : n().toString().substr(0,10),
-        oauth_signature_method : 'HMAC-SHA1',
-        oauth_version : '1.0'
-      };
-      console.log('required parameters: ', required_parameters, '*****');
+  /* We set the require parameters here */
+    var required_parameters = {
+      // oauth_consumer_key : process.env.oauth_consumer_key,
+      oauth_consumer_key : process.env.YELP_CONSUMER_KEY,
+      oauth_token : process.env.YELP_TOKEN,
+      oauth_nonce : n(),
+      oauth_timestamp : n().toString().substr(0,10),
+      oauth_signature_method : 'HMAC-SHA1',
+      oauth_version : '1.0'
+    };
+    console.log('required parameters: ', required_parameters, '*****');
 
-      /* We combine all the parameters in order of importance */
-      var parameters = _.assign(default_parameters, set_parameters, required_parameters);
+    /* We combine all the parameters in order of importance */
+    var parameters = _.assign(default_parameters, set_parameters, required_parameters);
 
-      /* We set our secrets here */
-      var consumerSecret = process.env.YELP_CONSUMER_SECRET;
-      var tokenSecret = process.env.YELP_TOKEN_SECRET;
+    /* We set our secrets here */
+    var consumerSecret = process.env.YELP_CONSUMER_SECRET;
+    var tokenSecret = process.env.YELP_TOKEN_SECRET;
 
-      /* Then we call Yelp's Oauth 1.0a server, and it returns a signature */
-      /* Note: This signature is only good for 300 seconds after the oauth_timestamp */
-      var signature = oauthSignature.generate(httpMethod, url, parameters, consumerSecret, tokenSecret, { encodeSignature: false});
+    /* Then we call Yelp's Oauth 1.0a server, and it returns a signature */
+    /* Note: This signature is only good for 300 seconds after the oauth_timestamp */
+    var signature = oauthSignature.generate(httpMethod, url, parameters, consumerSecret, tokenSecret, { encodeSignature: false});
 
-      /* We add the signature to the list of paramters */
-      parameters.oauth_signature = signature;
+    /* We add the signature to the list of paramters */
+    parameters.oauth_signature = signature;
 
-      /* Then we turn the paramters object, to a query string */
-      var paramURL = qs.stringify(parameters);
+    /* Then we turn the paramters object, to a query string */
+    var paramURL = qs.stringify(parameters);
 
-      /* Add the query string to the url */
-      var apiURL = url+'?'+paramURL;
-      console.log('apiURL: ', apiURL);
+    /* Add the query string to the url */
+    var apiURL = url+'?'+paramURL;
+    console.log('apiURL: ', apiURL);
 
-      /* Then we use request to send make the API Request */
-      // request(apiURL, function(error, response, body){
-      //   return callback(error, response, body);
-      request(apiURL, function(error, response, body){
-        return callback(error, response, body);
-      });
+    /* Then we use request to send make the API Request */
+
+    request(apiURL, function(error, response, body){
+      return callback(error, response, body);
+    });
 
     };
+
     request_yelp(set_parameters, callback);
   }
-}
